a954b0352a970c9e9c3ecde05dcc6b3f7ecd3983
gabreuvcr <gabreuvcr@gmail.com>
2020-06-19 16:52:00 -0300

    fix coding style

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function initChangeTheme() {
   function changeTheme(event) {
     event.preventDefault();
     map = document.querySelector('.mapa img');
+
     if (this.innerHTML === "Dark") {
       this.innerHTML = "Light";
       map.src = "img/mapa-dark.png";
@@ -9,11 +10,11 @@ function initChangeTheme() {
       this.innerHTML = "Dark";
       map.src = "img/mapa-light.png";
     }
+
     document.body.classList.toggle("light");
   }
   
   const theme = document.querySelector(".js-theme");
-  
   theme.addEventListener("click", changeTheme);
 }
 
@@ -21,7 +22,7 @@ function initTabNav() {
   const tabMenu = document.querySelectorAll('.js-tabmenu li');
   const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-  if(tabMenu.length && tabContent.length) {
+  if (tabMenu.length && tabContent.length) {
     function activeTab(index) {
       tabContent.forEach((section) => {
         section.classList.remove('ativo');  
@@ -33,12 +34,12 @@ function initTabNav() {
       itemMenu.addEventListener('click', () => activeTab(index));
     });
   }
-};
+}
 
 function initAccordion() {
   const accordionList = document.querySelectorAll('.js-accordion dt');
 
-  if(accordionList.length) {
+  if (accordionList.length) {
     function activeAccordion() {
       this.classList.toggle('ativo');
       this.nextElementSibling.classList.toggle('ativo');
@@ -71,25 +72,26 @@ function initSmoothScroll() {
   
   internalLinks.forEach((link) => {
     link.addEventListener('click', scrollToSection);
-  })
+  });
 }
 
 function initScrollAnimation() {
   const sections = document.querySelectorAll('.js-scroll');
-  if(sections.length) {
+  if (sections.length) {
     const windowHeight = window.innerHeight * 0.65; 
     
     function scrollAnimation() {
       sections.forEach((section) => {
         const sectionTop = section.getBoundingClientRect().top;
         const isSectionVisible = (sectionTop - windowHeight) < 0;
-        if(isSectionVisible) {
+        if (isSectionVisible) {
           section.classList.add('ativo');
         } else {
           section.classList.remove('ativo');
         }
       });
     }
+
     scrollAnimation();
     window.addEventListener('scroll', scrollAnimation);
   }
