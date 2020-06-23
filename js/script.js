5ea9fc102f49aed86a398f959b6cc6117ba86026
gabreuvcr <gabreuvcr@gmail.com>
2020-06-23 18:58:13 -0300

    added login pop-up

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initChangeTheme from './modules/theme.js';
 import initScrollAnimation from './modules/scroll-animation.js';
 import initAccordion from './modules/accordion.js';
 import initTabNavigation from './modules/tab-navigation.js';
+import initModal from './modules/modal.js';
 
 initChangeTheme();
 initSmoothScroll();
 initScrollAnimation();
 initAccordion();
 initTabNavigation();
+initModal();
