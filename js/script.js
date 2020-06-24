594ee9dbcf5f6477177f081a43e0002952012cc5
gabreuvcr <gabreuvcr@gmail.com>
2020-06-24 17:57:03 -0300

    added tooltip on the map

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ import initScrollAnimation from './modules/scroll-animation.js';
 import initAccordion from './modules/accordion.js';
 import initTabNavigation from './modules/tab-navigation.js';
 import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
 
 initChangeTheme();
 initSmoothScroll();
@@ -11,3 +12,4 @@ initScrollAnimation();
 initAccordion();
 initTabNavigation();
 initModal();
+initTooltip();
