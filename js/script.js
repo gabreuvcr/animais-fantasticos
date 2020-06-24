2ea49ca737371234c881b86b5d310fb4b3d69f96
gabreuvcr <gabreuvcr@gmail.com>
2020-06-24 19:11:47 -0300

    added dropdown menu

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ import initAccordion from './modules/accordion.js';
 import initTabNavigation from './modules/tab-navigation.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
 
 initChangeTheme();
 initSmoothScroll();
@@ -13,3 +14,4 @@ initAccordion();
 initTabNavigation();
 initModal();
 initTooltip();
+initDropdownMenu();
